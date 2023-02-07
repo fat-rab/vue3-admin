614994e484c0f4500bc4e6cdd0f7403ebe62ac80
fat-rab <1575478687@qq.com>
2023-02-07 20:03:27 +0800

    优化配置

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,6 @@ import {createPinia} from 'pinia'
 // import './styles/element/index.scss'
 // import ElementPlus from 'element-plus'
 
-// 如果需要使用ElMessage 还是需要引入(使用了unplugin-element-plus/vite,不再需要引入该样式)
-// import "element-plus/theme-chalk/src/message.scss"
-
 import './styles/index.scss'
 import request from './utils/request'
 import './permission'
@@ -20,10 +17,5 @@ import {mockXHR} from '../mock'
 mockXHR()
 
 const app = createApp(App).use(router).use(createPinia())
-// // 全局注册icon组件
-// import * as Icons from '@element-plus/icons-vue'
-// Object.keys(Icons).forEach(key => {
-//     app.component(key, Icons[key])
-// })
 app.config.globalProperties.$http = request
 app.mount('#app')  // 需要放到最后,否则无法配置app.config
