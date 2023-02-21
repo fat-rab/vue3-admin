b753e0bac4a5625265b0511ef345874f57c7a150
fat-rab <1575478687@qq.com>
2023-02-21 15:28:52 +0800

    优化

--- src/permission.ts
+++ src/permission.ts
@@ -27,7 +27,7 @@ router.beforeEach(async(to: RouteLocationNormalized, from: RouteLocationNormaliz
                     await userStore[UserActionEnum.GET_USER_INFO]()
                     const roles = userStore.roles
                     if (roles.length === 0) {
-                        return Promise.reject('roles.length===0')
+                        throw new Error('账号没有权限访问')
                     }
                     await permissionStore[PermissionActionsEnum.GET_PERMISSION_ROUTES](roles)
                     permissionStore.permissionRoutes.forEach((item) => {
